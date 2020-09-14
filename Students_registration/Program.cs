bbae997a45fc42605e2cc1dc0a6687d6f949edfc
tarikbilla <tarikbilla@gmail.com>
2020-09-14 15:00:49 +0600

    add other file

---
+++ Students_registration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Students_registration.CLASSES;
+using Students_registration.FORMS;
+
+namespace Students_registration
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            GLOBAL_VARS.d._server = "localhost";
+            GLOBAL_VARS.d._user = "root";
+            GLOBAL_VARS.d._pw = "";
+            GLOBAL_VARS.d._db = "students";
+            GLOBAL_VARS.d.connect();
+
+            Main m = new Main();
+            m.ShowDialog();
+        }
+    }
+}
